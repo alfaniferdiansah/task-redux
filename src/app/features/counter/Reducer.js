a2bb35132595ffea5c913489609dd75a68c00991
alfaniferdiansah <ferdiansah1808@gmail.com>
2023-02-01 17:22:45 +0700

    add redux

---
+++ src/app/features/counter/Reducer.js
@@ -0,0 +1,22 @@
+let InitialState = {
+    count: 0
+}
+
+const counterReducer = (state = InitialState, action) => {
+    switch (action.type) {
+        case 'INC':
+            return {
+                ...state,
+                count: state.count + action.value
+            }
+        case 'DEC':
+            return {
+                ...state,
+                count: state.count - action.value
+            }
+        default:
+            return state
+    }
+}
+
+export default counterReducer
